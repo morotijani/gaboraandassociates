ab38273dcc7801716fe2a92440d3f1486d591093
Tijani Moro <tjhackx111@gmail.com>
2025-03-17 13:44:30 +0000

    initial commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+	define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/jspence/');
+	
+	define('PROOT', '/jspence/');
+
+	define('FIXED_POUNDS_FIGURE', '7.75');
+	define('FIXED_CARAT_FIGURE_1', '10.51');
+	define('FIXED_CARAT_FIGURE_2', '52.838');
+	define('FIXED_TOTAL_FIGURE', '23');
+
+	// Define the idle timeout period (e.g., 5 minutes = 300 seconds)
+	define('IDLE_TIMEOUT', 600); // 600 seconds = 10 minutes
