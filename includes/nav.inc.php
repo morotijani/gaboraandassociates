9d5c17735f23a3599b620ad4fff48f58f3e0a232
Tijani Moro <tjhackx111@gmail.com>
2025-03-19 15:41:37 +0000

    initial commit

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -3,21 +3,21 @@
         <div class="container">
             <!-- Logo -->
             <div class="logo-wrapper">
-                <a class="logo" href="<?= PROOT; ?>"><img src="img/logo.png" class="logo-img" alt=""></a>
+                <a class="logo" href="<?= PROOT; ?>"><img src="<?= PROOT; ?>assets/media/logo.png" class="logo-img" alt=""></a>
             </div>
             <!-- Button -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"><i class="ti-menu"></i></span> </button>
             <!-- Menu -->
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav mx-auto">
-                    <li class="nav-item"> <a class="nav-link active" href="<?= PROOT; ?>">Home <i class="ti-angle-down"></i></a>
+                    <li class="nav-item"> <a class="nav-link active" href="<?= PROOT; ?>">Home</a>
                     </li>
-                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>about.html">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>practice-areas.html">Practice areas</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>case-study.html">Case Study</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>team.html">Attorneys</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>team.html">Blog</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>contact.html">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>about">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>practice-areas">Practice areas</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>case-study">Case Study</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>team">Attorneys</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>blog">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= PROOT; ?>contact">Contact</a></li>
                 </ul>
                 <div class="navbar-right">
                     <div class="button"><a href="<?= PROOT; ?>contact.html">Free consultant</a></div>
